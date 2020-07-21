b743ad724d4b0c74c3d5804d04205e60d6116585
gsnmahal <sa.gscbe@gmail.com>
2020-07-21 17:32:45 +0530

    Bug fix

---
+++ assets/js/slick-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      $('.slick-gallery').slick({
+          dots: true,
+          infinite: true,
+          speed: 300,
+          slidesToShow: 1,
+          adaptiveHeight: true
+      });
+    });
