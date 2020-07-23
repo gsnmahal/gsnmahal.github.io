9150988040549771f80dea0926bc47ab1cfb92d2
gsnmahal <sa.gscbe@gmail.com>
2020-07-23 16:32:30 +0530

    Update

--- assets/js/slick-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-      $('.slick-gallery').slick({
-          dots: true,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 1,
-          adaptiveHeight: true
-      });
-    });
